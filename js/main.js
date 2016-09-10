15f16df3e90848b6bfa9b68c87f00eb5fcb78247
rhoenig <rhoenig@hoenigwebdesign.com>
2016-09-10 16:11:00 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -1,23 +1,40 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+		var self = this;
+		this.store.findByName($('.search-key').val(), function(employees) {
+			$('.employee-list').html(self.employeeLiTpl(employees));
+		});
+	},
 
     initialize: function() {
-        this.store = new MemoryStore();
+		var self = this;
+		this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        this.store = new MemoryStore(function() {
+			self.showAlert('Store Initialized', 'Info');
+		});
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
+	
+	showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
+	
+	renderHomeView: function() {
+		$('body').html(this.homeTpl());
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
 
 };
 
-app.initialize();
+app.initialize: function() {
+    var self = this;
+    this.store = new MemoryStore(function() {
+        self.renderHomeView();
+    });
+}
