5c2465914d64b6381fecf7839dfe45af86345c46
rhoenig <rhoenig@hoenigwebdesign.com>
2016-09-10 16:32:37 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
 var app = {
 
     findByName: function() {
-		var self = this;
+        console.log('findByName');
+        var self = this;
 		this.store.findByName($('.search-key').val(), function(employees) {
 			$('.employee-list').html(self.employeeLiTpl(employees));
 		});
-	},
+    },
 
     initialize: function() {
 		var self = this;
-		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         this.store = new MemoryStore(function() {
 			self.showAlert('Store Initialized', 'Info');
+			self.renderHomeView();
 		});
+		this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+    },
 	
 	showAlert: function (message, title) {
 		if (navigator.notification) {
@@ -30,11 +32,7 @@ var app = {
 		$('.search-key').on('keyup', $.proxy(this.findByName, this));
 	},
 
+
 };
 
-app.initialize: function() {
-    var self = this;
-    this.store = new MemoryStore(function() {
-        self.renderHomeView();
-    });
-}
+app.initialize();
